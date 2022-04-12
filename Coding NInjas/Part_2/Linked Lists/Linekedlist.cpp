878ce371ebad0c8a95163002d3ad54cebd7c40cf
Ujjwal Pathak <92005669+ujjwalpathaak@users.noreply.github.com>
2022-04-12 15:21:57 +0530

    .

--- Coding NInjas/Part_2/Linked Lists/Linekedlist.cpp
+++ Coding NInjas/Part_2/Linked Lists/Linekedlist.cpp
@@ -1,121 +1,53 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <Stdio.h>
+#include <stdlib.h>
 
-class node
+struct node
 {
-public:
     int data;
-    node *next;
-
-    node(int data)
-    {
-        this->data = data;
-        next = NULL;
-    }
+    struct node *next;
 };
 
-/* node *takeinput() // node* type as we need head in return
+void reverse(struct node *head)
 {
-    int data;
-
-    cin >> data;
-    node *head = NULL;
-    while (data != -1)
+    struct node *curr = head, *prev = NULL, *nxt;
+    while (curr != NULL)
     {
-        node *newNode = new node(data);
-
-        if (head == NULL)
-        {
-            head = newNode;
-        }
-        else
-        {
-            node *temp = head;
-            while (temp->next != NULL)
-            {
-                temp = temp->next;
-            }
-
-            temp->next = newNode;
-        }
-
-        cin >> data;
+        nxt = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = nxt;
     }
-    return head;
-} */
-node *takeinput2() // node* type as we need head in return
-{
-    int data;
-
-    cin >> data;
-    node *head = NULL;
-    node *tail = NULL;
-    while (data != -1)
-    {
-        node *newNode = new node(data);
-
-        if (head == NULL)
-        {
-            head = newNode;
-            tail = newNode;
-        }
-        else
-        {
-            tail -> next = newNode;
-            tail = tail -> next;
-        }
-
-        cin >> data;
-    }
-    return head;
+    head = prev;
 }
 
-void print(node *head)
+void traverseLL(struct node *ptr)
 {
-    node *temp = head;
-
-    while (temp != 0)
+    while (ptr != 0)
     {
-        cout << temp->data << " ";
-        temp = temp->next;
+        printf("%d -> ", ptr->data);
+        ptr = ptr->next;
     }
-    cout << endl;
+    printf(" NULL\n");
 }
+
 int main()
 {
-    /* node *head3 = takeinput();
-    print(head3); */
-    node *head3 = takeinput2();
-    print(head3);
-    
-    // Statically
-        node n1(1);
-        node *head = &n1;
-        node n2(2);
-        node n3(3);
-        node n4(4);
-        node n5(5);
+    struct node *head = (struct node *)malloc(sizeof(struct node));
+    struct node *one = (struct node *)malloc(sizeof(struct node));
+    struct node *two = (struct node *)malloc(sizeof(struct node));
+    struct node *three = (struct node *)malloc(sizeof(struct node));
 
-        n1.next = &n2;
-        n2.next = &n3;
-        n3.next = &n4;
-        n4.next = &n5;
-    // Dynamically
-    node *n6 = new node(10);
-    node *head2 = n6;
-    node *n7 = new node(20);
-    node *n8 = new node(30);
-    node *n9 = new node(40);
-    node *n10 = new node(50);
-
-        n6 -> next = n7;
-        n7 -> next = n8;
-        n8 -> next = n9;
-        n9 -> next = n10;
-
-    print(head);
-    print(head2);
-   
+    head->data = 10;
+    head->next = one;
+    one->data = 20;
+    one->next = two;
+    two->data = 30;
+    two->next = three;
+    three->data = 40;
+    three->next = NULL;
+    traverseLL(head);
+    reverse(head);
+    traverseLL(head);
 
     return 0;
 }
